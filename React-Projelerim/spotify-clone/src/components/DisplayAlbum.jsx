ea9d49ea36f35f7900004a2b3e8280d6a2f13c35
Enes İlgezdi <enesilgezdi@gmail.com>
2024-08-07 02:03:02 +0300

    spotify clone

--- React-Projelerim/spotify-clone/src/components/DisplayAlbum.jsx
+++ React-Projelerim/spotify-clone/src/components/DisplayAlbum.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets } from '../assets/assets';
+import { albumsData, assets, songsData } from '../assets/assets';
+import { PlayerContext } from '../context/PlayerContext';
 
 function DisplayAlbum() {
 
     const { id } = useParams();
     const albumData = albumsData[id];
+    const { playWithId } = useContext(PlayerContext)
 
     return (
         <>
@@ -27,6 +29,27 @@ function DisplayAlbum() {
 
                 </div>
             </div>
+            <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
+                <p><b className='mr-4'>#</b>Title</p>
+                <p>Album</p>
+                <p className='hidden sm:block'>Date Added</p>
+                <img className='m-auto w-4' src={assets.clock_icon} alt="" />
+            </div>
+            <hr />
+            {
+                songsData.map((item, index) => (
+                    <div onClick={() => playWithId(item.id)} key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+                        <p className='text-white'>
+                            <b className='mr-4 text-[#a7a7a7]'>{index + 1}</b>
+                            <img className='inline w-10 mr-5' src={item.image} alt="" />
+                            {item.name}
+                        </p>
+                        <p className='text-[15px]'>{albumData.name}</p>
+                        <p className='text-[15px] hidden sm:block'>5 days ago</p>
+                        <p className='text-[15px] text-center'>{item.duration}</p>
+                    </div>
+                ))
+            }
         </>
     )
 }
