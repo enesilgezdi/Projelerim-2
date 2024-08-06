4192fcc73ce8f8ab1eb70224d26b19f905ce17b9
Enes İlgezdi <enesilgezdi@gmail.com>
2024-08-06 15:58:31 +0300

    spotify clone

---
+++ React-Projelerim/spotify-clone/src/components/DisplayAlbum.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { useParams } from 'react-router-dom'
+import { albumsData, assets } from '../assets/assets';
+
+function DisplayAlbum() {
+
+    const { id } = useParams();
+    const albumData = albumsData[id];
+
+    return (
+        <>
+            <Navbar />
+            <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
+                <img className='w-48 rounded' src={albumData.image} alt="" />
+                <div className='flex flex-col'>
+                    <p>Playlist</p>
+                    <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
+                    <h4>{albumData.desc}</h4>
+                    <p className='mt-1'>
+                        <img className='inline-block w-5' src={assets.spotify_logo} alt="" />
+                        <b> Spotify</b>
+                        • 1,321,588 likes
+                        • <b>50 songs,</b>
+                        about 2 hr 30 min
+                    </p>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DisplayAlbum
